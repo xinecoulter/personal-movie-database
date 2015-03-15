221f0536626225a388116c9cacda03f6ba6dd627
Christine Coulter <christinetcoulter@gmail.com>
2015-03-15 19:52:01 -0400

    Create movie_genres join table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315233758) do
+ActiveRecord::Schema.define(version: 20150315234807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20150315233758) do
   create_table "movie_directors", force: true do |t|
     t.integer  "movie_id"
     t.integer  "director_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "movie_genres", force: true do |t|
+    t.integer  "movie_id"
+    t.integer  "genre_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
