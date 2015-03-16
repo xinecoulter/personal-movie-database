de83909cd2fd21ce0345bf6d35513e179538a0d0
Christine Coulter <christinetcoulter@gmail.com>
2015-03-15 20:26:50 -0400

    Create Genre and MovieGenre models. Set relationships between Movie, Genre, and MovieGenre.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ActiveRecord::Base
   belongs_to :user
   has_many :movie_directors
   has_many :directors, through: :movie_directors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
