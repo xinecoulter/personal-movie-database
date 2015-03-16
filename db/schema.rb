c784affd848d28c95fc9a2f814bdf4018dab4a05
Christine Coulter <christinetcoulter@gmail.com>
2015-03-15 21:43:31 -0400

    Create actors table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315234807) do
+ActiveRecord::Schema.define(version: 20150316013904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "directors", force: true do |t|
     t.string   "name"
