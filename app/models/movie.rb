adefd1cd5396d1a35dbc9ee1ff69fb35c925c426
Christine Coulter <christinetcoulter@gmail.com>
2015-03-15 21:52:11 -0400

    Create Actor and MovieActor models. Set relationships between Movie, Actor, and MovieActor models.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,6 @@ class Movie < ActiveRecord::Base
   has_many :directors, through: :movie_directors
   has_many :movie_genres
   has_many :genres, through: :movie_genres
+  has_many :movie_actors
+  has_many :actors, through: :movie_actors
 end
