d1ded52b13b722f4e0d5c0c528aeb6d8c124e9e8
Christine Coulter <christinetcoulter@gmail.com>
2015-04-07 00:53:39 -0400

    Create ImdbData model. Handles getting imdb data for a given imdb id, assigning imdb data to a Movie object, and creating Director, Actor, and Genre objects.

---
+++ app/models/imdb_data.rb
@@ -0,0 +1,79 @@
+class ImdbData
+  def initialize(imdb_id)
+    @data = Imdb::Movie.new(imdb_id)
+  end
+
+  def title
+    @data.title
+  end
+
+  def company
+    @data.company
+  end
+
+  def length
+    @data.length
+  end
+
+  def plot
+    @data.plot
+  end
+
+  def plot_summary
+    @data.plot_summary
+  end
+
+  def poster
+    @data.poster
+  end
+
+  def year
+    @data.year
+  end
+
+  def writers
+    @data.writers
+  end
+
+  def characters
+    @data.cast_members_characters
+  end
+
+  def save_directors(movie)
+    @data.director.each do |director|
+      movie.directors << Director.find_or_create_by!(name: director)
+    end
+  end
+
+  def save_actors(movie)
+    @data.cast_members.each do |actor|
+      movie.actors << Actor.find_or_create_by!(name: actor)
+    end
+  end
+
+  def save_genres(movie)
+    @data.genres.each do |genre|
+      movie.genres << Genre.find_or_create_by!(name: genre)
+    end
+  end
+
+  def convert_to_movie(movie)
+    ActiveRecord::Base.transaction do
+      movie.title = title
+      movie.company = company
+      movie.length = length
+      movie.plot = plot
+      movie.plot_summary = plot_summary
+      movie.poster = poster
+      movie.year = year
+      movie.writers = writers
+      movie.characters = characters
+      movie.save!
+
+      save_directors(movie)
+      save_actors(movie)
+      save_genres(movie)
+    end
+    movie
+  end
+end
