2b891afff0a6a7796eb135f395e7a9dfc3c20685
Christine Coulter <christinetcoulter@gmail.com>
2015-04-07 00:49:01 -0400

    Add basic movie new view and MoviesController#new. Search for movie by title and render results. Include form for each result to save it as the user's movie. Add attr_accessor attributes to Movie model.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,16 @@ class MoviesController < ApplicationController
     @movies = current_user.movies
   end
 
+  def new
+    if params[:movie]
+      @search_results = Imdb::Search.new(movie_params[:search]).movies
+    end
+  end
+
+private
+
+  def movie_params
+    params.require(:movie).permit(:search, :imdb_search_id, :storage_identification)
+  end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,7 @@ class Movie < ActiveRecord::Base
   has_many :genres, through: :movie_genres
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  attr_accessor :search, :imdb_search_id
+
 end
