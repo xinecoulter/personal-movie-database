2fcb961c49e429db1b7602dec67528539b33bde8
Christine Coulter <christinetcoulter@gmail.com>
2015-05-10 00:13:04 -0400

    Write MoviesController destroy action.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -37,6 +37,12 @@ class MoviesController < ApplicationController
     end
   end
 
+  def destroy
+    movie = Movie.find(params[:id])
+    movie.destroy!
+    redirect_to authenticated_root_path
+  end
+
 private
 
   def movie_params
