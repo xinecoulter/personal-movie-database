168c4faac381d8a54b8e8dd7d2fa4d84cfd41b33
Christine Coulter <christinetcoulter@gmail.com>
2015-05-22 01:37:36 -0400

    Write Actor#role_in to retrieve an actor's role associated with a specified movie and invoke it in the movies show view.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,8 @@
 class Actor < ActiveRecord::Base
   has_many :roles
   has_many :movies, through: :roles
+
+  def role_in(movie)
+    roles.find_by_movie_id(movie.id)
+  end
 end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -15,4 +15,20 @@ describe Actor do
     actor.save!
     assert("Benedict Cumberbatch" == actor.reload.name)
   end
+
+  describe "#role_in" do
+    let(:movie){create(:movie)}
+    subject{actor.role_in(movie)}
+    context "when the actor has a role in a movie" do
+      let(:role){create(:role, actor: actor, movie: movie)}
+      it "returns the role" do
+        assert(role == subject)
+      end
+    end
+    context "when the actor has no role in a movie" do
+      it "returns false" do
+        assert(subject.nil?)
+      end
+    end
+  end
 end
